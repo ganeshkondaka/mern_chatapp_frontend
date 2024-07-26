77538195f287954d137c294726be4fa5e190c497
ganeshkondaka <kondakaganesh2003@outlook.com>
2024-07-26 16:41:13 +0530

    removed the .. from image src path

--- src/components/Myinsta.jsx
+++ src/components/Myinsta.jsx
@@ -5,7 +5,7 @@ export default function Myinsta() {
         <div className='instadiv'>
             <div className='instaicon'>
                 <a href="https://www.instagram.com/_dark.el?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
-                    <img src="../src/assets/instalogo.png" alt="instalogo!!" />
+                    <img src="/src/assets/instalogo.png" alt="instalogo!!" />
                 </a>
                 {/* <p>instagram</p> */}
             </div>
